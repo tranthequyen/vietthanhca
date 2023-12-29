66bfdeb553c2d7cf78d21f737aab2b5a0c4782cc
Quang Phúc <phucquang9988@gmail.com>
2023-12-29 10:34:13 +0700

    p

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import { publicRoutes } from './routes';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import { DefaultLayout } from './layout';
+import { Fragment } from 'react';
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Routes>
+                {publicRoutes.map((route, index) => {
+                    const Layout = route.layout ? DefaultLayout : Fragment
+                    const Page = route.component
+                    return <Route
+                        key={index}
+                        path={route.path}
+                        element={
+                            <Layout>
+                                <Page />
+                            </Layout>
+                        }
+                    />
+                })}
+            </Routes>
+        </Router>
+    );
 }
 
 export default App;
